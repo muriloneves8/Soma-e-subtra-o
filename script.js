81b37a894cd40eaa317d0503101a747550d5b030
Murilo <muriloneeves8@gmail.com>
2022-11-26 15:35:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+        let num1 = parseInt(prompt("digite um numero"));
+        let num2 = parseInt(prompt("digite um numero"));
+
+        let resultado = num1 - num2;
+        alert(`${num1} - ${num2}=${resultado}`)
+    
